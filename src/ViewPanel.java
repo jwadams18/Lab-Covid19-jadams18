51de0196b38dd486414ed0df601f692af97268f9
Jadams0 <59323501+Jadams0@users.noreply.github.com>
2020-04-20 23:33:53 -0500

    Draws the county outlines, and colors all counties in same state random color.

--- src/ViewPanel.java
+++ src/ViewPanel.java
@@ -4,13 +4,17 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 public class ViewPanel extends JPanel {
 
     private Controller c;
+    private HashMap<Integer, County> countyList;
 
     public ViewPanel() {
+        this.setPreferredSize(new Dimension(1000, 1000));
         this.c = new Controller();
+        countyList = c.getCounties();
 
     }
 
@@ -18,6 +22,6 @@ public class ViewPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        countyList.forEach((fips, county) -> county.draw(g));
     }
 }
