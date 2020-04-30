6188b6bb8e84a69d82c822f70282d8be62bff498
Jadams0 <59323501+Jadams0@users.noreply.github.com>
2020-04-30 14:38:24 -0500

    Added the gui controls to the bottom of the JFrame

--- src/Starter.java
+++ src/Starter.java
@@ -13,8 +13,19 @@ public class Starter {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(1000, 1000));
         frame.setVisible(true);
+        frame.setLayout(new GridLayout());
 
         ViewPanel vpanel = new ViewPanel();
+        vpanel.setMinimumSize(new Dimension(925, 925));
+        guiControlPanel panel = new guiControlPanel(vpanel.getController());
+
+        JSplitPane spane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, vpanel, panel);
+        spane.setResizeWeight(1);
+        spane.setDividerSize(5);
+
+
+        frame.add(spane);
+        frame.pack();
 
     }
 }
