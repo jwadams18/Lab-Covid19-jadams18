4f3202ec27e1b587528e02dae6be117c78db3ebe
Jadams0 <59323501+Jadams0@users.noreply.github.com>
2020-04-30 14:34:23 -0500

    Added the ability to change the slider to change the date, and added ActionListeners for the radio buttons to display either cases or deaths.

--- src/guiControlPanel.java
+++ src/guiControlPanel.java
@@ -4,6 +4,9 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class guiControlPanel extends JPanel {
 
@@ -12,24 +15,55 @@ public class guiControlPanel extends JPanel {
     private JRadioButton cases, deaths;
     private ButtonGroup buttonGroup;
     private GridBagConstraints constraints;
+    private Controller c;
+    private int oldSliderValue = 0, deltaSlider;
 
-    public guiControlPanel() {
+    public guiControlPanel(Controller c) {
 
+        this.c = c;
         constraints = new GridBagConstraints();
-        currentDate = new JLabel("Current date");
+        currentDate = new JLabel();
+        currentDate.setText("Current Date: " + c.getFirstDate());
 
         constraints.gridx = 1;
         this.add(currentDate, constraints);
 
-        dateSlider = new JSlider(0, 100, 50);
+        dateSlider = new JSlider(0, c.getNumDays(), 0);
         constraints.gridx = 0;
         this.add(dateSlider, constraints);
 
         dateSlider.addChangeListener(event -> {
-            currentDate.setText(dateSlider.getValue() + " temp");
+            //Getting the number of days to change by, should be either +1 or -1
+            deltaSlider = dateSlider.getValue() - oldSliderValue;
+            oldSliderValue = dateSlider.getValue();
+            //If the date hasnt been set, then it must be the first date
+            if (c.getRelativeDate() == null) {
+                c.setRelativeDate(c.getFirstDate());
+            }
+
+            String oldDate = c.getRelativeDate();
+            System.out.println("Date before Addition: " + oldDate);
+            //Specifying date format that matches the given date
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Calendar cal = Calendar.getInstance();
+            try {
+                //Setting the date to the given date
+                cal.setTime(sdf.parse(oldDate));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            //Number of Days to add
+            cal.add(Calendar.DAY_OF_MONTH, deltaSlider);
+            //Date after adding the days to the given date
+            String newDate = sdf.format(cal.getTime());
+            //Displaying the new Date after addition of Days
+            System.out.println("Date after Addition: " + newDate);
+            c.setRelativeDate(newDate);
+            currentDate.setText("Current Date: " + newDate);
         });
 
-        currentDisplay = new JLabel("Currently displaying ");
+        currentDisplay = new JLabel("Currently displaying:  ");
 
         constraints.gridx = 2;
         this.add(currentDisplay, constraints);
@@ -46,6 +80,14 @@ public class guiControlPanel extends JPanel {
 
         constraints.gridx = 4;
         this.add(deaths, constraints);
+
+        cases.addActionListener(event -> {
+            c.setDisplay(0);
+        });
+
+        deaths.addActionListener(event -> {
+            c.setDisplay(1);
+        });
 
     }
 }
