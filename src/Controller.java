111edba01af1d5729f6f664f1637a37dc9ef2f54
Jadams0 <59323501+Jadams0@users.noreply.github.com>
2020-04-30 14:37:47 -0500

    Started to implement the drawing of circles representing cases/deaths to population based on current date in slider.

--- src/Controller.java
+++ src/Controller.java
@@ -2,11 +2,49 @@
 //    Lab-COVID-jadams18
 //    Controller.java
 
+import java.util.HashMap;
+
 public class Controller {
 
     private Model m;
 
     public Controller() {
         this.m = new Model();
+
+    }
+
+    public void setRelativeDate(String relativeDate) {
+        m.relativeDate = relativeDate;
+    }
+
+    public String getRelativeDate() {
+        return m.relativeDate;
+    }
+
+
+    public HashMap<Integer, County> getCounties() {
+        return m.countiesList;
+    }
+
+    public void load() {
+        m.loadCounties();
+        m.loadVirusData();
+    }
+
+    public String getFirstDate() {
+        return m.firstDate;
+    }
+
+    public int getNumDays() {
+        m.calcNumDays();
+        return m.numDays;
+    }
+
+    public void setDisplay(int display) {
+        m.currentDisplay = display;
+    }
+
+    public int getDisplay() {
+        return m.currentDisplay;
     }
 }
